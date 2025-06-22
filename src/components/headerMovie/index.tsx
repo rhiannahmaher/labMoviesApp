dc7eef53103635cc4742dab270098caed73476fa
Rhiannah Maher <20085527@mail.wit.ie>
2025-06-22 16:35:28 +0100

    Exercise - Show faourite icon on Move Details page.

--- src/components/headerMovie/index.tsx
+++ src/components/headerMovie/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 import Paper from "@mui/material/Paper";
@@ -6,6 +6,8 @@ import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
 import HomeIcon from "@mui/icons-material/Home";
 import { MovieDetailsProps } from "../../types/interfaces"; 
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import Avatar from "@mui/material/Avatar";
 
 const styles = {
     root: {  
@@ -15,10 +17,20 @@ const styles = {
     flexWrap: "wrap",
     padding: 1.5,
   },
+  avatar: {
+    backgroundColor: "rgb(255, 0, 0)", // Red color for the heart icon
+  }
 };
 
 const MovieHeader: React.FC<MovieDetailsProps> = (movie) => {
-  
+  const [displayFavourite, setDisplayFavourite] = useState(false); 
+
+  useEffect(() => {
+    const storedFavourites = JSON.parse(localStorage.getItem("favourites") || "[]"); // When headerMovie component initially loads, it checks local storage for favourited movies.
+    const matched = storedFavourites.some((fav: any) => fav.id === movie.id); // Returns true or false.
+    setDisplayFavourite(matched); // Updates displayFavourite state (true/false).
+  }, [movie.id]);
+
   return (
     <Paper component="div" sx={styles.root}>
       <IconButton aria-label="go back">
@@ -26,6 +38,14 @@ const MovieHeader: React.FC<MovieDetailsProps> = (movie) => {
       </IconButton>
 
       <Typography variant="h4" component="h3">
+        {displayFavourite && ( // If setDisplayFavourite is true, component is re-rendered and icon is displayed.
+          <Avatar sx={styles.avatar}>
+            <FavoriteIcon />
+          </Avatar>
+        )} 
+      </Typography>
+      
+      <Typography variant="h4" component="h3"> 
         {movie.title}{"   "}
         <a href={movie.homepage}>
           <HomeIcon color="primary"  fontSize="large"/>
